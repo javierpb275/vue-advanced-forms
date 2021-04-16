fc348cbe1ba3e341aee67437b8fc26f419593855
javierpb275 <javierpb275@gmail.com>
2021-04-16 16:33:57 +0200

    defined data of activities view

--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -1,5 +1,35 @@
 <template>
     <div class="mt-5">
-        
+
     </div>
 </template>
+
+<script>
+export default {
+   data() {
+       return {
+           //This does not belong to the form data:
+           reset: true,
+           activity_types_components: [
+               'basket', 'football', 'tennis'
+           ],
+           //activity: This belongs to the form data that we are gonna interact with
+           activity: {
+               terms: false,
+               personal_data: {
+                   name: '',
+                   surname: ''
+               },
+               activity_type_selection: {
+                   type: 1,
+                   date: ''
+               },
+               activity_selected_data: {
+                   team: '',
+                   information: ''
+               }
+           }
+       }
+   }
+}
+</script>
