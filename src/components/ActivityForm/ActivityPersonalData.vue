993af2009fede10f7148c5eea63a0c04270d482c
javierpb275 <javierpb275@gmail.com>
2021-04-16 17:48:34 +0200

    ActivityPersonalData component created. created mixin validation. imported both in Activity view and used here. passed data personal_data to the ActivityPersonalData component as props

---
+++ src/components/ActivityForm/ActivityPersonalData.vue
@@ -0,0 +1,44 @@
+<template>
+    <b-card title="Personal Data" class="mt-3">
+        <b-row>
+            <b-col>
+                <b-form-group label="Name">
+                    <b-form-input autocomplete="off"
+                    v-model="personal_data.name"
+                    v-validate="'required|min:2'"
+                    :state="$parent.validateState('name')"
+                    name="name" placeholder="Name...">
+                    </b-form-input>
+<!--$parent: access the parent (component that displays this component) to the validateState() method that we get from the mixins-->
+                    <b-form-invalid-feedback>
+                        {{errors.first('name')}}
+                    </b-form-invalid-feedback>
+                </b-form-group>
+            </b-col>
+            <b-col>
+                <b-form-group label="Surname">
+                    <b-form-input autocomplete="off"
+                    v-model="personal_data.surname"
+                    v-validate="'required|min:4'"
+                    :state="$parent.validateState('surname')"
+                    name="surname" placeholder="Surname...">
+                    </b-form-input>
+                    <b-form-invalid-feedback>
+                        {{errors.first('surname')}}
+                    </b-form-invalid-feedback>
+                </b-form-group>
+            </b-col>
+        </b-row>
+    </b-card>
+</template>
+
+<script>
+export default {
+    props: {
+        personal_data: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/Activities.vue
+++ src/views/Activities.vue
@@ -1,11 +1,26 @@
 <template>
-    <div class="mt-5">
-
-    </div>
+    <b-form @submit.prevent="submitActivity" class="mb-5">
+        <activity-personal-data :personal_data="activity.personal_data" ></activity-personal-data>
+        <b-row>
+            <b-col>
+                <!--this runs submitActivity() since it is type submit-->
+                <b-btn variant="primary" type="submit">
+                    Submit
+                </b-btn>
+            </b-col>
+        </b-row>
+    </b-form>
 </template>
 
 <script>
+import ActivityPersonalData from '../components/ActivityForm/ActivityPersonalData';
+import validationMixin from '../mixins/validation';
+
 export default {
+    components: {
+        ActivityPersonalData
+    },
+    mixins: [validationMixin],
    data() {
        return {
            //This does not belong to the form data:
@@ -29,6 +44,11 @@ export default {
                    information: ''
                }
            }
+       }
+   },
+   methods: {
+       async submitActivity() {
+           const validate = await this.$validator.validateAll();
        }
    }
 }
